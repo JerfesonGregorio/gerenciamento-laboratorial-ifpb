00446fb4408e886099b6743bf4d4d1989408172e
João Gabriel Costa <joaogabrielcosta2004@gmail.com>
2026-04-28 16:11:00 -0300

    config: backend

---
+++ backend/laboratorios/src/test/java/com/ifpb/gerenciamento/laboratorios/LaboratoriosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifpb.gerenciamento.laboratorios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaboratoriosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
