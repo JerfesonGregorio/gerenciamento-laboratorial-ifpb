2299a9b0e7778eb0350d733d1199af2475be1856
João Gabriel Costa <joaogabrielcosta2004@gmail.com>
2026-05-14 17:37:49 +0000

    primeira commit

--- backend/laboratorios/src/main/java/com/ifpb/gerenciamento/laboratorios/entity/Laboratorio.java
+++ backend/laboratorios/src/main/java/com/ifpb/gerenciamento/laboratorios/entity/Laboratorio.java
@@ -1,5 +1,21 @@
 package com.ifpb.gerenciamento.laboratorios.entity;
 
-public class Laboratorio {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "")
+public class Laboratorio {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private Long id;
+
+    private String nome;
+
+    private String email;
+
+    private String senha;
 }
